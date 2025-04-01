6c07a7536b884c5ff12f03c26f5ba66d12fc26bf
Shaury Gautam <gautamsm@dukes.jmu.edu>
2025-04-01 11:00:09 -0400

    fixes

--- library/settings.py
+++ library/settings.py
@@ -46,7 +46,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'django_app.urls'
+ROOT_URLCONF = 'library.urls'
 
 TEMPLATES = [
     {
@@ -64,7 +64,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'django_app.wsgi.application'
+WSGI_APPLICATION = 'library.wsgi.application'
 
 
 # Database
